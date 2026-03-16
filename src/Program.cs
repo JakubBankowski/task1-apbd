d29067401948165945e8f1da84f740af87462e22
JakubBankowski <s33203@pjwstk.edu.pl>
2026-03-16 22:19:33 +0100

    Added Average Function

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,12 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Person person = new Person("Jakub", 21);
+        
+    }
+    
+    public int CalculateAverage(int[] values)
+    {
+        return values.Sum() / values.Length;
     }
 }
