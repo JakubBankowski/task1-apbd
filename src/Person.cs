5ed976eb836b7d4e0e3512d2b65106984dfcedf4
JakubBankowski <s33203@pjwstk.edu.pl>
2026-03-16 21:43:08 +0100

    Added class Person

---
+++ src/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace;
+
+public class Person
+{
+    private string name;
+    int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
